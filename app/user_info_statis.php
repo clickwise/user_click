356dd088df4b680afc21a2c73d0da1ef6f018fab
clickwise <strongmind666@126.com>
2014-07-02 11:04:12 +0800

    modify app

--- app/user_info_statis.php
+++ app/user_info_statis.php
@@ -25,15 +25,21 @@ class user_info_statis extends app_base{
 	}
 
 	function prepare(){
-          
-           $user_se_keywords_day="/user/nstat/$this->day/user_se_keywords_day";
-           $user_se_keywords_day_seg="/user/nstat/$this->day/user_admatch/user_se_keywords_day_seg";
-           $this->seg_keywords($user_se_keywords_day,$user_se_keywords_day_seg);   
-          
-           $user_se_keywords_day_wordstatis="/user/nstat/$this->day/user_admatch/user_se_keywords_day_wordstatis";
-           $this->seg_word_statis($user_se_keywords_day_seg,$user_se_keywords_day_wordstatis);           
 
-            
+		$user_se_keywords_day="/user/nstat/$this->day/user_se_keywords_day";
+		$user_se_keywords_day_seg="/user/nstat/$this->day/user_admatch/user_se_keywords_day_seg";
+		//$this->seg_keywords($user_se_keywords_day,$user_se_keywords_day_seg);   
+
+		$user_se_keywords_day_wordstatis="/user/nstat/$this->day/user_admatch/user_se_keywords_day_wordstatis";
+		//$this->seg_word_statis($user_se_keywords_day_seg,$user_se_keywords_day_wordstatis);           
+
+		$user_se_keywords_day_wordstatis_tl="/user/nstat/$this->day/user_admatch/user_se_keywords_day_wordstatis_tl";
+		//$this->seg_word_tl($user_se_keywords_day_wordstatis,$user_se_keywords_day_wordstatis_tl);
+
+		$user_se_keywords_day_wordstatis_keys="/user/nstat/$this->day/user_admatch/user_se_keywords_day_wordstatis_keys";
+		$this->seg_word_keys($user_se_keywords_day_wordstatis_tl,$user_se_keywords_day_wordstatis_keys);       
+
+
 	}
 
 	function execute(){
@@ -45,38 +51,73 @@ class user_info_statis extends app_base{
 
 
 
-        function seg_keywords($user_se_keywords_day,$user_se_keywords_day_seg)
-        {
-           $this->ha_utils->remove_hdfs($user_se_keywords_day_seg);
+	function seg_keywords($user_se_keywords_day,$user_se_keywords_day_seg)
+	{
+		$this->ha_utils->remove_hdfs($user_se_keywords_day_seg);
 
-           $cmd = " jar smart_hadoop.jar cn.clickwise.user_click.seg.SegMR 4 3 $user_se_keywords_day $user_se_keywords_day_seg";
-          
-           $ret = $this->ha_utils->run_raw($cmd);
-           if($ret == false){
-                   $this->error("Can not finish seg keywords:".$this->day);
-                   return false;
-           }
+		$cmd = " jar smart_hadoop.jar cn.clickwise.user_click.seg.SegMR 4 3 $user_se_keywords_day $user_se_keywords_day_seg";
 
-           return $ret;
-        }   
- 
+		$ret = $this->ha_utils->run_raw($cmd);
+		if($ret == false){
+			$this->error("Can not finish seg keywords:".$this->day);
+			return false;
+		}
 
-        function seg_word_statis($user_se_keywords_day_seg,$user_se_keywords_day_wordstatis)
-        {
-           $this->ha_utils->remove_hdfs($user_se_keywords_day_wordstatis);
+		return $ret;
+	}   
 
-           $cmd = " jar smart_hadoop.jar cn.clickwise.user_click.seg.LineWordCountMR 4 3 $user_se_keywords_day_seg $user_se_keywords_day_wordstatis";
 
-           $ret = $this->ha_utils->run_raw($cmd);
-           if($ret == false){
-                   $this->error("Can not finish word_statis:".$this->day);
-                   return false;
-           }
+	function seg_word_statis($user_se_keywords_day_seg,$user_se_keywords_day_wordstatis)
+	{
+		$this->ha_utils->remove_hdfs($user_se_keywords_day_wordstatis);
 
-           return $ret;
+		$cmd = " jar smart_hadoop.jar cn.clickwise.user_click.seg.LineWordCountMR 4 3 $user_se_keywords_day_seg $user_se_keywords_day_wordstatis";
 
-        }       
-        
+		$ret = $this->ha_utils->run_raw($cmd);
+		if($ret == false){
+			$this->error("Can not finish word_statis:".$this->day);
+			return false;
+		}
+
+		return $ret;
+
+	}       
+
+
+	function seg_word_tl($user_se_keywords_day_wordstatis,$user_se_keywords_day_wordstatis_tl) 
+	{
+		$this->ha_utils->remove_hdfs($user_se_keywords_day_wordstatis_tl);
+
+		$cmd = " jar smart_hadoop.jar cn.clickwise.user_click.seg.FieldProcessMR 4 1 $user_se_keywords_day_wordstatis $user_se_keywords_day_wordstatis_tl STR2LONGTIME";
+
+		$ret = $this->ha_utils->run_raw($cmd);
+		if($ret == false){
+			$this->error("Can not finish word_statis_tl:".$this->day);
+			return false;
+		}
+
+		return $ret;
+
+	}
+
+
+	function seg_word_keys($user_se_keywords_day_wordstatis_tl,$user_se_keywords_day_wordstatis_keys)
+	{
+		$this->ha_utils->remove_hdfs($user_se_keywords_day_wordstatis_keys);
+
+		$cmd = " jar smart_hadoop.jar cn.clickwise.user_click.seg.FieldProcessMR 4 2 $user_se_keywords_day_wordstatis_tl $user_se_keywords_day_wordstatis_keys STRMAP BD_KEYS";
+
+		$ret = $this->ha_utils->run_raw($cmd);
+		if($ret == false){
+			$this->error("Can not finish word_statis_keys:".$this->day);
+			return false;
+		}
+
+		return $ret;
+
+	}
+
+
 
 
 };
