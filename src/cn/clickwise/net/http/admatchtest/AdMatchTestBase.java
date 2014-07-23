5ac0a142f2feb65bbd569ffb4df5c66155616c4d
clickwise <strongmind666@126.com>
2014-07-23 14:29:41 +0800

    host cate

--- src/cn/clickwise/net/http/admatchtest/AdMatchTestBase.java
+++ src/cn/clickwise/net/http/admatchtest/AdMatchTestBase.java
@@ -9,6 +9,7 @@ public class AdMatchTestBase{
 	
 	public HttpClientTool hct=new HttpClientTool();
 	//public String url_prefix="http://106.187.35.172:8000";
-	public String url_prefix="http://192.168.110.186:8000";
+	//public String url_prefix="http://192.168.110.186:8000";
+	public String url_prefix="http://42.62.29.25:8000";
 	
 }
