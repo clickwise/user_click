bbe3fb528a1d487ae30672f6f779a4e62ff8cd7b
bluesky1999 <strongmind666@126.com>
2014-09-22 15:59:48 +0800

    separate build

--- make.sh
+++ make.sh
@@ -1,2 +1,32 @@
 #!/bin/bash
-/usr/local/ant/bin/ant
+
+echo $1
+ANT_BUILD=/usr/local/ant/bin/ant
+echo $ANT_BUILD
+OPT=1
+if [ "$1" = "user_click" ]
+then 
+ echo "build user_click";
+ $ANT_BUILD -buildfile build.xml
+else
+ OPT=1
+fi
+
+if [[ "$1" = "tool" ]]
+then
+ echo "build tool";
+ $ANT_BUILD -buildfile build_tool.xml
+else
+ OPT=2
+fi
+
+if [ $1 = "feathouse" ]
+then
+ echo "build feathouse";
+ $ANT_BUILD -buildfile build_feathouse.xml
+else
+ OPT=3
+fi
+
+
+
