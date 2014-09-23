6e68746117d5cf454c81cbae904414ac894e77be
clickwise <strongmind666@126.com>
2014-09-23 17:24:03 +0800

    run test

--- src/cn/clickwise/clickad/classify/ClassifierUseText.java
+++ src/cn/clickwise/clickad/classify/ClassifierUseText.java
@@ -13,6 +13,7 @@ import java.util.List;
 import java.util.StringTokenizer;
 import java.util.regex.Pattern;
 
+import org.jmlp.classify.svm_struct.source.LABEL;
 import org.jmlp.classify.svm_struct.source.svm_struct_api_factory;
 import org.jmlp.classify.svm_struct.source.svm_struct_classify;
 import org.slf4j.Logger;
@@ -27,8 +28,9 @@ import cn.clickwise.liqi.str.basic.SSO;
 
 /**
  * 对普通文本进行分类
+ * 
  * @author zkyz
- *
+ * 
  */
 public class ClassifierUseText {
 
@@ -54,32 +56,21 @@ public class ClassifierUseText {
 		}
 	}
 
-
 	public String getSample(String filter_content) throws Exception {
-		
-		String sample = "";	
-		
+
+		String sample = "";
+
 		/*
-		String seg_s = seg.segAnsi(filter_content);
-		seg_s = seg_s.trim();
-		if (seg_s.equals("")) {
-			return "";
-		}
+		 * String seg_s = seg.segAnsi(filter_content); seg_s = seg_s.trim(); if
+		 * (seg_s.equals("")) { return ""; }
+		 * 
+		 * String tag_s = posTagger.tag(seg_s); tag_s = tag_s.trim(); if
+		 * (tag_s.equals("")) { return ""; }
+		 * 
+		 * String key_s = ""; key_s = ke.keyword_extract(tag_s); key_s =
+		 * key_s.trim(); if (key_s.equals("")) { return ""; }
+		 */
 
-		String tag_s = posTagger.tag(seg_s);
-		tag_s = tag_s.trim();
-		if (tag_s.equals("")) {
-			return "";
-		}
-
-		String key_s = "";
-		key_s = ke.keyword_extract(tag_s);
-		key_s = key_s.trim();
-		if (key_s.equals("")) {
-			return "";
-		}
-        */
-			
 		sample = get_word_id(filter_content);
 		sample = sample.trim();
 		if (sample.equals("")) {
@@ -140,30 +131,28 @@ public class ClassifierUseText {
 		}
 		return res;
 	}
-
-
-
+    /*
 	public String cate(String line) {
 		String cate_name = "";
 		try {
-			
+
 			String sample = getSample(line);
-			//Label label_pre = docate(sample);
-			Label label_pre =null;
+			// Label label_pre = docate(sample);
+			Label label_pre = null;
 			cate_name = getCateName(label_pre);
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return cate_name;
 	}
-
-	public String getCateName(Label y) {
+    */
+	public String getCateName(LABEL y) {
 		String cate_name = "";
-		int tempid = y.first_class;
-		
-		//if ((tempid >= 1) && (tempid <= NUM_CLASS)) {
-		if ((tempid >= 1) ) {
+		int tempid = y.class_index;
+
+		// if ((tempid >= 1) && (tempid <= NUM_CLASS)) {
+		if ((tempid >= 1)) {
 			cate_name = label_names.get(tempid + "") + "";
 		} else {
 			cate_name = "NA";
@@ -177,10 +166,6 @@ public class ClassifierUseText {
 		double weight;
 	}
 
-	public class Label {
-		int first_class;
-		double score;
-	}
 
 	public HashMap getDictFromStream(String input_file) {
 		// TODO Auto-generated method stub
@@ -294,60 +279,180 @@ public class ClassifierUseText {
 
 	public static void main(String[] args) throws Exception {
 
-		svm_struct_classify ssc=new svm_struct_classify();	
-		if(args.length>0)
+
+		if (args.length < 3||args.length>5) {
+			System.err.println("Usage:ClassifierUseText [<api_type>] [<model>] <field_num> <seg_field_index> <separator>");
+			System.err.println("    api_type: svm struct api type for example:multiclass(0)");
+			System.err.println("    model: model save path");
+			System.err.println("    dict : 外加词典路径");
+			System.err.println("    field_num : 输入的字段个数");
+			System.err.println("    seg_field_index: 要分词的字段编号，从0开始，即0表示第一个字段");
+			System.err.println("    separator:字段间的分隔符，001 表示 字符001，blank 表示\\s+ 即连续空格");
+			System.exit(1);
+		}
+		
+        //分类的体系，一层分类、二层分类等,0表示一层分类，1表示细化的两层分类
+		int api_type=0;
+		
+		//模型文件
+		String model="";
+		
+		//输入的字段个数用
+		int fieldNum=0;
+		
+		//待分词的字段编号
+		int segFieldIndex=0;
+		
+		//字段间的分隔符:001 表示 \001
+		//             :blank 表示\\s+ 即连续空格
+		String separator="";
+		String outputSeparator="";
+		
+		
+		if(args.length==3)
 		{
-			if(args[0].equals("-help"))
+			fieldNum=Integer.parseInt(args[0]);
+			segFieldIndex=Integer.parseInt(args[1]);
+			if(args[2].equals("001"))
 			{
-			  System.out.println("Usage:ClassifierUseText [<api_type>] [<model>] \n"
-					+ " api_type: svm struct api type for example:multiclass, \n"
-					+ " model: model save path \n");
-	          System.exit(1);
+				separator="\001";
+				outputSeparator="\001";
+			}
+			else if(args[2].equals("blank"))
+			{
+				separator="\\s+";
+				outputSeparator="\t";
+			}
+			else
+			{
+				separator=args[2].trim();
+				outputSeparator=separator.trim();
+			}		
+		}
+		else if(args.length==4)
+		{
+		    if(Pattern.matches("[\\d]+", args[0]))
+		    {
+		    	api_type=Integer.parseInt(args[0]);
+		    }
+		    else
+		    {
+		    	model=args[0];
+		    }
+		    
+			fieldNum=Integer.parseInt(args[1]);
+			segFieldIndex=Integer.parseInt(args[2]);
+			if(args[3].equals("001"))
+			{
+				separator="\001";
+				outputSeparator="\001";
+			}
+			else if(args[3].equals("blank"))
+			{
+				separator="\\s+";
+				outputSeparator="\t";
+			}
+			else
+			{
+				separator=args[3];
+				outputSeparator=separator.trim();
+			}
+		}
+		else if(args.length==5)
+		{
+			api_type=Integer.parseInt(args[0]);
+			model=args[1];
+			fieldNum=Integer.parseInt(args[2]);
+			segFieldIndex=Integer.parseInt(args[3]);
+			if(args[4].equals("001"))
+			{
+				separator="\001";
+				outputSeparator="\001";
+			}
+			else if(args[4].equals("blank"))
+			{
+				separator="\\s+";
+				outputSeparator="\t";
+			}
+			else
+			{
+				separator=args[4];
+				outputSeparator=separator.trim();
 			}
 		}
 		
-		ArrayList<String> sample_list=new ArrayList<String>();
-		InputStreamReader isr=new InputStreamReader(System.in);
-		BufferedReader br=new BufferedReader(isr);	
-		ClassifierUseText cut=new ClassifierUseText();
-				
-		String key_line="";
-		String docid="";
-		String docwords="";
+		svm_struct_classify ssc = new svm_struct_classify();
 		
-		String[] pairArr=null;
-		while((key_line=br.readLine())!=null)
+		if (args.length == 3) {
+			String default_model_file = "model_host";
+			// ssc.classify_from_arraylist(sample_list,default_model_file);
+			ssc.init_svm_struct(default_model_file);
+		} else if (args.length == 4) {// default: multiclass
+
+			// 选用何种分类体系
+			svm_struct_api_factory ssaf = new svm_struct_api_factory(api_type);
+			ssc.init_svm_struct(model);
+			
+			
+			// ssc.classify_from_arraylist(sample_list,args[0]);
+
+		} else if (args.length == 5) {
+
+			// 选用何种分类体系
+			svm_struct_api_factory ssaf = new svm_struct_api_factory(api_type);
+			ssc.init_svm_struct(model);
+			// ssc.classify_from_arraylist(sample_list,args[1]);
+		}
+		
+		ClassifierUseText cut = new ClassifierUseText();
+
+
+
+		InputStreamReader isr=new InputStreamReader(System.in);
+		BufferedReader br=new BufferedReader(isr);
+		
+		OutputStreamWriter osw=new OutputStreamWriter(System.out);
+		PrintWriter pw=new PrintWriter(osw);
+		
+		String line="";
+		String[] fields=null;
+		while((line=br.readLine())!=null)
 		{
-			pairArr=SSO.sepFirst(key_line, "\\s+");
-			if(pairArr==null)
+			fields=line.split(separator);
+			if(fields.length!=fieldNum)
 			{
 				continue;
 			}
-			sample_list.add(pairArr[0]+" "+cut.get_word_id(pairArr[1]));
-			logger.info(pairArr[0]+" "+cut.get_word_id(pairArr[1]));
-		}
-		
-		
-		if(args.length==0)
-		{
-			String default_model_file="model_host";
-			ssc.classify_from_arraylist(sample_list,default_model_file);
-		}
-		else if (args.length == 1) {// default: multiclass
+			for(int j=0;j<segFieldIndex;j++)
+			{
+				pw.print(fields[j]+outputSeparator);
+			}
+			if(segFieldIndex<(fieldNum-1))
+			{
+		    	pw.print(cut.getCateName(ssc.classifyWordString(cut.get_word_id(fields[segFieldIndex]))).trim()+outputSeparator);
+			}
+			else
+			{
+				pw.print(cut.getCateName(ssc.classifyWordString(cut.get_word_id(fields[segFieldIndex]))).trim());
+			}
 			
-			//选用何种分类体系
-			svm_struct_api_factory ssaf = new svm_struct_api_factory(0);			
-			ssc.classify_from_arraylist(sample_list,args[0]);
-		
-
-		} else if (args.length == 2) {
-
-			//选用何种分类体系
-			svm_struct_api_factory ssaf = new svm_struct_api_factory(Integer.parseInt(args[0]));
-			ssc.classify_from_arraylist(sample_list,args[1]);
+			for(int j=segFieldIndex+1;j<fieldNum-1;j++)
+			{
+				pw.println(fields[j]+outputSeparator);
+			}
+			
+			if(segFieldIndex<(fieldNum-1))
+			{
+				pw.print(fields[fieldNum-1]);
+			}	
+			pw.println();
 		}
+		
+		isr.close();
+		osw.close();
+		br.close();
+		pw.close();
+
 	}
-	
-	
-	
+
 }
