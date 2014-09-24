ca576547c3d8a741070cd5516af6d1b8d9fa2bde
clickwise <strongmind666@126.com>
2014-09-24 21:00:31 +0800

    run test

--- src/cn/clickwise/rpc/CommandHandler.java
+++ src/cn/clickwise/rpc/CommandHandler.java
@@ -1,11 +1,15 @@
 package cn.clickwise.rpc;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
+import java.io.PrintWriter;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
@@ -19,27 +23,33 @@ public abstract class CommandHandler implements HttpHandler{
 		//System.out.println("request:"+request);
 		
 		InputStream is = exchange.getRequestBody();
-
-		DataInputStream dis=new DataInputStream(is);
-
-		
-		Command cmd=null;
-		try{
-			ObjectInputStream ois =new ObjectInputStream(dis);
-			System.out.println(ois.toString());
-			cmd=(Command)ois.readObject();
-			System.out.println("ok");
-			
-		}
-		catch(Exception e)
-		{
-			System.out.println("error");
-			e.printStackTrace();
-		}
-		
+        InputStreamReader isr=new InputStreamReader(is);
+        BufferedReader br=new BufferedReader(isr);
+        String line="";
+        String content="";
+        while((line=br.readLine())!=null)
+        {
+        	content+=line;
+        	
+        }
+        try{
+        FileWriter fw=new FileWriter("/tmp/test_serial.txt");
+        PrintWriter pw=new PrintWriter(fw);
+        pw.println(content);
+        FileInputStream fis = new FileInputStream("/tmp/test_serial.txt");
+        ObjectInputStream ois = new ObjectInputStream(fis);
+		Command result=(Command)ois.readObject();
+		FileStatusCommand fresult=(FileStatusCommand)result;
+		System.out.println("get fresult name:"+fresult.getName());
+		System.out.println("get fresult path:"+fresult.getPath());
+        }
+        catch(Exception e)
+        {
+        	e.printStackTrace();
+        }
 		//exchange.setAttribute("", "");
-		cmd=deserialization(is);
-		complie(cmd,exchange);
+		//cmd=deserialization(is);
+		//complie(cmd,exchange);
 	}
 
 	
