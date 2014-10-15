ece93a16e93de9d4016a5a6a03a802021cb6e87a
clickwise <strongmind666@126.com>
2014-10-15 16:08:26 +0800

    test

--- src/cn/clickwise/clickad/radiusClient/PacketHead.java
+++ src/cn/clickwise/clickad/radiusClient/PacketHead.java
@@ -75,7 +75,7 @@ public class PacketHead {
         int mask=0xff;  
         int temp=0;  
         int n=0;  
-        for(int i=0;i<4;i++){  
+        for(int i=0;i<b.length;i++){  
            n<<=8;  
            temp=b[i]&mask;  
            n|=temp;  
