cdd8e83736e4d90ca2cf16552e431f8499fd8374
clickwise <strongmind666@126.com>
2014-10-20 17:36:41 +0800

    hive statistic by keys rpc

--- src/cn/clickwise/rpc/HiveStatisticByKeysHandler.java
+++ src/cn/clickwise/rpc/HiveStatisticByKeysHandler.java
@@ -1,15 +1,110 @@
 package cn.clickwise.rpc;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.URI;
+
+import cn.clickwise.lib.linux.COMMAND;
+import cn.clickwise.lib.string.SSO;
 
 import com.sun.net.httpserver.HttpExchange;
 
 public class HiveStatisticByKeysHandler extends Handler{
 
 	@Override
-	public void handle(HttpExchange arg0) throws IOException {
-		// TODO Auto-generated method stub
+	public void handle(HttpExchange exchange) throws IOException {
+		URI uri = exchange.getRequestURI();
+		System.out.println("uri:" + uri);
+		HiveStatisticByKeysCommand hskc = HiveStatisticByKeysCommand.readObject(SSO
+				.afterStr(uri.toString(), "method="));
+		complie(hskc, exchange);
 		
 	}
 
+	public void complie(Command cmd, HttpExchange exchange) {
+
+		HiveStatisticByKeysCommand hskc = (HiveStatisticByKeysCommand) cmd;
+
+		InputStream is = exchange.getRequestBody();
+		InputStreamReader isr = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(isr);
+		try {
+			FileWriter fw = new FileWriter(hskc.getRemoteTmpPath());
+			PrintWriter pw = new PrintWriter(fw);
+
+			String line = "";
+			while ((line = br.readLine()) != null) {
+				pw.println(line);
+			}
+
+			fw.close();
+			pw.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+        //COMMAND.exec(HadoopCmd.mkParent(hfkc.getHdfTmpPath()));
+
+			
+		try {
+			exchange.sendResponseHeaders(200, 0);
+			OutputStream os = exchange.getResponseBody();
+
+			File resDir = new File(hskc.getResultRemotePath());
+			File[] subFiles = resDir.listFiles();
+
+			for (int j = 0; j < subFiles.length; j++) {
+				if(!(FileName.isValidResult(subFiles[j])))
+				{
+					continue;
+				}
+				FileInputStream resfis = new FileInputStream(subFiles[j]);
+				InputStreamReader resisr = new InputStreamReader(resfis);
+				BufferedReader resbr = new BufferedReader(resisr);
+
+				String resline = "";
+				while ((resline = resbr.readLine()) != null) {
+					os.write(new String(resline + "\n").getBytes());
+				}
+				resfis.close();
+				resisr.close();
+				resbr.close();
+			}
+			cleanTmpWorkplace(hskc);
+			os.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	public void cleanTmpWorkplace(HiveStatisticByKeysCommand hskc)
+	{
+		if(SSO.tioe(hskc.getTmpIdentify()))
+		{
+			return;
+		}
+		File remoteTmp=new File(hskc.getRemoteTmpPath());
+		
+		if(remoteTmp.getName().indexOf(hskc.getTmpIdentify())>-1)
+		{
+			remoteTmp.delete();
+		}
+		
+		File resultRemote=new File(hskc.getResultRemotePath());
+		if(resultRemote.getName().indexOf(hskc.getTmpIdentify())>-1)
+		{
+			COMMAND.exec(FileCommand.deleteDirectory(resultRemote.getAbsolutePath()));
+		}
+		
+	}
+	
 }
