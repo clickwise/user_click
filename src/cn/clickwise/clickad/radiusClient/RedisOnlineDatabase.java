1361f495ca60d3862b8b93fcf6c283182ba419bc
clickwise <strongmind666@126.com>
2014-10-28 15:59:23 +0800

    test

--- src/cn/clickwise/clickad/radiusClient/RedisOnlineDatabase.java
+++ src/cn/clickwise/clickad/radiusClient/RedisOnlineDatabase.java
@@ -2,55 +2,51 @@ package cn.clickwise.clickad.radiusClient;
 
 import redis.clients.jedis.Jedis;
 
-public class RedisOnlineDatabase extends OnlineDatabase{
+public class RedisOnlineDatabase extends OnlineDatabase {
 
-	private Jedis jedis=null;
-	
+	private Jedis jedis = null;
+
 	@Override
 	public void connect(RedisCenter rc) {
-		
+
 		jedis = new Jedis(rc.getIp(), rc.getPort(), 1000);
 		jedis.select(Integer.parseInt(rc.getDatabase()));
 		jedis.ping();
-		
+
 	}
 
 	@Override
 	public void update(RecordLight rec) {
-	
-		if(rec.getAcctStatusType()==1)//上线
+
+		if (rec.getFramedIpAddress() == null || rec.getUserName() == null) {
+			return;
+		}
+		if (rec.getAcctStatusType() == 1)// 上线
 		{
 			jedis.set(rec.getFramedIpAddress(), rec.getUserName());
 			jedis.set(rec.getUserName(), rec.getFramedIpAddress());
-		}
-		else if(rec.getAcctStatusType()==2)//下线
+		} else if (rec.getAcctStatusType() == 2)// 下线
 		{
 			jedis.del(rec.getFramedIpAddress());
 			jedis.del(rec.getUserName());
-		}
-		else if(rec.getAcctStatusType()==3)//心跳
+		} else if (rec.getAcctStatusType() == 3)// 心跳
 		{
-			String oldUserName=jedis.get(rec.getFramedIpAddress());
-			String oldIp=jedis.get(rec.getUserName());
-			
-			//ip对应的用户名是否发生变化
-			if(!(oldUserName.equals(rec.getUserName())))
-			{
-			  jedis.set(rec.getFramedIpAddress(), rec.getUserName());
+			String oldUserName = jedis.get(rec.getFramedIpAddress());
+			String oldIp = jedis.get(rec.getUserName());
+
+			// ip对应的用户名是否发生变化
+			if ((oldUserName == null)
+					|| (!(oldUserName.equals(rec.getUserName())))) {
+				jedis.set(rec.getFramedIpAddress(), rec.getUserName());
+			} else {
+
 			}
-			else
-			{
-				
-			}
-			
-			//用户名对应的ip是否发生变化
-			if(!(oldIp.equals(rec.getFramedIpAddress())))
-			{
-			jedis.set(rec.getUserName(), rec.getFramedIpAddress());
-			}
-			else
-			{
-				
+
+			// 用户名对应的ip是否发生变化
+			if ((oldIp == null) || (!(oldIp.equals(rec.getFramedIpAddress())))) {
+				jedis.set(rec.getUserName(), rec.getFramedIpAddress());
+			} else {
+
 			}
 		}
 	}
