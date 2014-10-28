dd8ba86485faa8e7992c46f4a1671ebc7617a488
clickwise <strongmind666@126.com>
2014-10-28 18:32:20 +0800

    run test

--- src/cn/clickwise/redis/RedisTestOrigin.java
+++ src/cn/clickwise/redis/RedisTestOrigin.java
@@ -7,11 +7,11 @@ public class RedisTestOrigin {
 	public static void main(String[] args)
 	{
 		Jedis jedis = new Jedis("180.96.26.203", 6379, 1000);// redis服务器地址
-		jedis.select(10);
+		//jedis.select(10);
 		jedis.ping();
 		//jedis.set("md5_cb696edca40ed45e4533dc6c12cd5d30", "单肩");
-		System.out.println(jedis.get("jhnjlhZ5b+KFZUPjuKTVjA"));
-		//180.109.191.116 3       jhnjlhZ5b+KFZUPjuKTVjA==        2014-10-28 15:42:49
+		System.out.println(jedis.get("iHtEieImICyxtpYu4+ugwQ=="));
+		//222.187.184.103 1       iHtEieImICyxtpYu4+ugwQ==        2014-10-28 18:25:17
 		
 	}
 	
