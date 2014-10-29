fcbc143f44763a62a40ebaf58e8e8939fbf3bdf5
clickwise <strongmind666@126.com>
2014-10-29 09:41:35 +0800

    run test

--- src/cn/clickwise/clickad/radiusClient/QueueRecordPond.java
+++ src/cn/clickwise/clickad/radiusClient/QueueRecordPond.java
@@ -186,7 +186,14 @@ public class QueueRecordPond extends RecordPond {
 		 */
 		public void parseHexRecord() {
 
+			int ti=0;
 			while (true) {
+				ti++;
+				if(ti>1000)
+				{
+					Thread ct=Thread.currentThread();
+					ct.stop();
+				}
 				try {
 					String record = pollFromPond();
 					if (SSO.tioe(record)) {
