9eb222c9dbcd5975fd1ca7ecb8d1d1ed872c46fb
clickwise <strongmind666@126.com>
2014-10-30 10:44:22 +0800

    query server

--- src/cn/clickwise/clickad/radiusClient/QueryEasyServer.java
+++ src/cn/clickwise/clickad/radiusClient/QueryEasyServer.java
@@ -4,63 +4,54 @@ import java.net.InetSocketAddress;
 
 import redis.clients.jedis.Jedis;
 
-
 import com.sun.net.httpserver.HttpServer;
 
 /**
- * 接收RTB传过来的用户数据查询请求，
- * 查询cassandra数据库，返回结果
+ * 接收RTB传过来的用户数据查询请求， 查询cassandra数据库，返回结果
+ * 
  * @author zkyz
  */
-public class QueryEasyServer extends Server{
+public class QueryEasyServer extends Server {
 
-
-	
 	private ConfigureFactory confFactory;
-	private int serverPort=0;
-	
-	public void init()
-	{
-		confFactory=ConfigureFactoryInstantiate.getConfigureFactory();
+	private int serverPort = 0;
+
+	public void init() {
+		confFactory = ConfigureFactoryInstantiate.getConfigureFactory();
 	}
-	
+
 	@Override
 	public void run() {
 		init();
-		
-		try{
-		HttpServer hs = HttpServer.create(new InetSocketAddress(confFactory.getServerPort()), 0);
-		
-		Context[] contexts=null;
-		Handler[] handlers=null;
-		contexts=confFactory.getContext();
-		handlers=confFactory.getHandler();
-		Jedis jedis = new Jedis(confFactory.getRedisIp(), confFactory.getRedisPort(), 1000);// redis服务器地址
-		
-		for(int i=0;i<handlers.length;i++)
-		{
-			handlers[i].setJedis(jedis);
-		}
-		//jedis.select(10);
-		jedis.ping();
-		
-		for(int i=0;i<contexts.length;i++)
-		{
-			hs.createContext(contexts[i].getName(), handlers[i]);
-		}
-		hs.setExecutor(null);
-		hs.start();
-		}
-		catch(Exception e)
-		{
+
+		try {
+			HttpServer hs = HttpServer.create(
+					new InetSocketAddress(confFactory.getServerPort()), 0);
+
+			Context[] contexts = null;
+			Handler[] handlers = null;
+			contexts = confFactory.getContext();
+			handlers = confFactory.getHandler();
+			Jedis jedis = new Jedis(confFactory.getRedisIp(),
+					confFactory.getRedisPort(), 1000);// redis服务器地址
+			jedis.ping();
+
+			System.out.println(jedis.get("49.84.32.216"));
+			for (int i = 0; i < handlers.length; i++) {
+				handlers[i].setJedis(jedis);
+			}
+			// jedis.select(10);
+
+			for (int i = 0; i < contexts.length; i++) {
+				hs.createContext(contexts[i].getName(), handlers[i]);
+			}
+			hs.setExecutor(null);
+			hs.start();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-	
-		
-	}
-	
 
+	}
 
 	public ConfigureFactory getConfFactory() {
 		return confFactory;
@@ -69,18 +60,14 @@ public class QueryEasyServer extends Server{
 	public void setConfFactory(ConfigureFactory confFactory) {
 		this.confFactory = confFactory;
 	}
-	
 
-	
-	public static void main(String[] args)
-	{
-		if(args.length!=1)
-		{
+	public static void main(String[] args) {
+		if (args.length != 1) {
 			System.err.println("Usage:QueryEasyServer <port>");
 			System.exit(1);
 		}
-		
-		QueryEasyServer es=new QueryEasyServer();
+
+		QueryEasyServer es = new QueryEasyServer();
 		es.setServerPort(Integer.parseInt(args[0]));
 		Thread easyThread = new Thread(es);
 		easyThread.start();
@@ -93,7 +80,5 @@ public class QueryEasyServer extends Server{
 	public void setServerPort(int serverPort) {
 		this.serverPort = serverPort;
 	}
-
-
 
 }
