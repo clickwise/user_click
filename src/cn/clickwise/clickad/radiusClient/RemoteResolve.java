35522e1691acd1bd3d5b74182fbe96bc685794cb
clickwise <strongmind666@126.com>
2014-11-05 18:14:15 +0800

    test

--- src/cn/clickwise/clickad/radiusClient/RemoteResolve.java
+++ src/cn/clickwise/clickad/radiusClient/RemoteResolve.java
@@ -1,59 +1,64 @@
 package cn.clickwise.clickad.radiusClient;
 
-
 import java.io.DataInputStream;
 import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 /**
- * server 端
- * 接收另一台机器传过来的未完全解析的包，
- * 进行解析并存入redis中
+ * server 端 接收另一台机器传过来的未完全解析的包， 进行解析并存入redis中
+ * 
  * @author zkyz
  */
 public class RemoteResolve {
 
 	private Socket sock;
 
-	
 	private ConfigureFactory confFactory;
-		
-	private static 	QueueRecordPond queuePond=new QueueRecordPond();
+
+	private static QueueRecordPond queuePond = new QueueRecordPond();
 	
-	public void init()
-	{
+	private ArrayList<Thread> startedThread=new ArrayList<Thread>();
+
+	public void init() {
 		setConfFactory(ConfigureFactoryInstantiate.getConfigureFactory());
 		startPond(3);
-		try{
-			ServerSocket sk = new ServerSocket(confFactory.getRSPort()); 
-			Socket connectFromClient = null; 
-			
-			while(true)
-			{
-				connectFromClient=sk.accept();
-				ServerThread st=new ServerThread(connectFromClient);
-				Thread t=new Thread(st);
-				t.setDaemon(true);
-				t.start();	
+		try {
+			ServerSocket sk = new ServerSocket(confFactory.getRSPort());
+			Socket connectFromClient = null;
+			int tn = 0;
+			while (true) {
+				tn++;
+				try {
+					connectFromClient = sk.accept();
+					ServerThread st = new ServerThread(connectFromClient);
+					Thread t = new Thread(st);
+					t.setDaemon(true);
+					System.out.println("start thread " + tn);
+					t.start();
+					startedThread.add(t);
+					if(tn>5)
+					{
+						for(int j=0;j<startedThread.size()-5;j++)
+						{
+							startedThread.get(j).stop();
+						}
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 			}
-			
-	
-		}catch(Exception e)
-		{
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-	
-		
-		
+
 	}
-	
-	public void startPond(int threadNum)
-	{
+
+	public void startPond(int threadNum) {
 		queuePond.startConsume(threadNum);
 	}
-	
 
 	public Socket getSock() {
 		return sock;
@@ -63,8 +68,6 @@ public class RemoteResolve {
 		this.sock = sock;
 	}
 
-	
-	
 	public ConfigureFactory getConfFactory() {
 		return confFactory;
 	}
@@ -73,65 +76,69 @@ public class RemoteResolve {
 		this.confFactory = confFactory;
 	}
 
-
-	private class ServerThread 	implements Runnable{
+	private class ServerThread implements Runnable {
 
 		private Socket connectFromClient;
-		
+
 		private DataInputStream sockIn;
-		
-		public ServerThread(Socket sock)
-		{
+
+		public ServerThread(Socket sock) {
 			super();
-			connectFromClient=sock;
-			try{
-				InputStream in=sock.getInputStream();
-				sockIn=new DataInputStream(in);
-			
-				//sockIn.
-			}
-			catch(Exception e)
-			{
+			connectFromClient = sock;
+			try {
+				InputStream in = sock.getInputStream();
+				sockIn = new DataInputStream(in);
+
+				// sockIn.
+			} catch (Exception e) {
+				try {
+					sockIn.close();
+				} catch (Exception ee) {
+
+				}
 				e.printStackTrace();
 			}
-	
+
 		}
-		
+
 		@Override
 		public void run() {
-			
-			while(true)
-			{
-				try{
-				  int len=sockIn.readInt();
-				  if(len<0)
-				  {
-					  //Thread.sleep(1);
-					  continue;
-				  }
-				  String str="";
-				  for(int j=0;j<len;j++)
-				  {
-					  str+=sockIn.readChar();
-				  }
-				 // System.out.println("len:"+len+" str:"+str);
-				  queuePond.add2Pond(str);
-				}
-				catch(Exception e)
-				{
-					//e.printStackTrace();
+
+			while (true) {
+				try {
+					int len = sockIn.readInt();
+					if (len < 0) {
+						// Thread.sleep(1);
+						continue;
+					}
+					String str = "";
+					for (int j = 0; j < len; j++) {
+						str += sockIn.readChar();
+					}
+					// System.out.println("len:"+len+" str:"+str);
+					queuePond.add2Pond(str);
+				} catch (Exception e) {
+					// e.printStackTrace();
 				}
 			}
-			
+
 		}
-		
+
+		public void finalize() {
+			try {
+				connectFromClient.close();
+				sockIn.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
 	}
-	
-	public static void main(String[] args)
-	{
-		RemoteResolve rs=new RemoteResolve();
+
+	public static void main(String[] args) {
+		RemoteResolve rs = new RemoteResolve();
 		rs.init();
-		
+
 	}
-	
+
 }
