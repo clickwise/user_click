48736f223f4927166955808c5782b1913e1940b3
clickwise <strongmind666@126.com>
2014-11-12 14:13:44 +0800

    test

--- src/cn/clickwise/clickad/profile/udf/SegmentUdf.java
+++ src/cn/clickwise/clickad/profile/udf/SegmentUdf.java
@@ -1,22 +1,33 @@
 package cn.clickwise.clickad.profile.udf;
 
 import org.apache.hadoop.hive.ql.exec.UDF;
+import org.jmlp.str.app.SetJar;
+
+import cn.clickwise.lib.string.SSO;
 
 public class SegmentUdf extends UDF {
 	
-	
+	private static final SetJar setJar=new SetJar();
 	
     public String evaluate(String str) {
 
         try {
-
-            return "HelloWorld " + str;
+        	if(SSO.tioe(str))
+        	{
+        		return "";
+        	}
+        	
+            String seg_str=setJar.double_seg(str);
+            if(SSO.tioe(seg_str))
+            {
+            	return "";
+            }
+            
+            return seg_str;
 
         } catch (Exception e) {
-
-        	
-            return null;
         }
+        return "";
 
     }
     
