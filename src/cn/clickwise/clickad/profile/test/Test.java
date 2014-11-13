2b11a3817de378d462f3a5749868f81bf19387ed
clickwise <strongmind666@126.com>
2014-11-13 14:56:28 +0800

    run test

---
+++ src/cn/clickwise/clickad/profile/test/Test.java
@@ -0,0 +1,26 @@
+package cn.clickwise.clickad.profile.test;
+
+import java.io.IOException;
+
+import org.ansj.splitWord.analysis.CRFAnalysis;
+
+import com.ansj.vec.Word2VEC;
+
+public class Test {
+    public static void main(String[] args) throws IOException {
+        Word2VEC w1 = new Word2VEC() ;
+        w1.loadGoogleModel("library/corpus.bin") ;
+        
+        System.out.println(w1.distance("奥尼尔"));
+        
+        System.out.println(w1.distance("毛泽东"));
+        
+        System.out.println(w1.distance("邓小平"));
+        
+        
+        System.out.println(w1.distance("魔术队"));
+        
+        System.out.println(w1.distance("魔术"));
+        
+    }
+}
