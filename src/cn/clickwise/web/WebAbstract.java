3b1994db78329e4ce89aa71cd244783044bc8356
clickwise <strongmind666@126.com>
2014-11-20 15:36:17 +0800

    run test

--- src/cn/clickwise/web/WebAbstract.java
+++ src/cn/clickwise/web/WebAbstract.java
@@ -1,16 +1,19 @@
 package cn.clickwise.web;
 
+import cn.clickwise.lib.string.SSO;
+
 /**
- * 网页的抽象 
+ * 网页的抽象
+ * 
  * @author zkyz
- *
+ * 
  */
 public class WebAbstract {
 
 	private String title;
-	
+
 	private String keywords;
-	
+
 	private String description;
 
 	public String getTitle() {
@@ -36,15 +39,35 @@ public class WebAbstract {
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	
-	public String toString()
-	{
-		String str="";
-		str=str+" title:"+title+"\n"
-			   +" keywords:"+keywords+"\n"
-			   +" description:"+description;
+
+	public String toRegularString() {
 		
+		String rstr="";
+		if(SSO.tioe(title))
+		{
+			title="";
+		}
+		
+		if(SSO.tioe(keywords))
+		{
+			keywords="";
+		}
+		
+		if(SSO.tioe(description))
+		{
+			description="";
+		}
+		
+		
+		return title+"\t"+keywords+"\t"+description;
+	}
+
+	public String toString() {
+		String str = "";
+		str = str + " title:" + title + "\n" + " keywords:" + keywords + "\n"
+				+ " description:" + description;
+
 		return str;
 	}
-	
+
 }
--- src/cn/clickwise/web/WebTest.java
+++ src/cn/clickwise/web/WebTest.java
@@ -1,61 +1,125 @@
 package cn.clickwise.web;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Set;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
+import com.ansj.vec.domain.WordEntry;
+
 public class WebTest {
 
-	public String getTitle(String url)
-	{
-		Document doc=null;
+	private Fetcher fetcher;
+
+	public void init() {
+		fetcher = new Fetcher();
+	}
+
+	public String getTitle(String url) {
+		Document doc = null;
 		try {
 
-			//////String content=fetcher.getSourceEasyProxy(url,getProxy());
-			//////doc=Jsoup.parse(content);
+			// ////String content=fetcher.getSourceEasyProxy(url,getProxy());
+			// ////doc=Jsoup.parse(content);
 			doc = Jsoup.connect(url).get();
-			if(doc==null)
-			{
+			if (doc == null) {
 				return null;
 			}
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return doc.title();
 	}
-	
-	public WebAbstract getAbstract(String url)
-	{
-		Document doc=null;
-		WebAbstract wa=null;
+
+	public WebAbstract getAbstract(String url) {
+		Document doc = null;
+		WebAbstract wa = null;
 		try {
 
-			//////String content=fetcher.getSourceEasyProxy(url,getProxy());
-			//////doc=Jsoup.parse(content);
+			// ////String content=fetcher.getSourceEasyProxy(url,getProxy());
+			// ////doc=Jsoup.parse(content);
 			doc = Jsoup.connect(url).get();
-			if(doc==null)
-			{
+			if (doc == null) {
 				return null;
 			}
-			
+
 			wa.setTitle(doc.title());
-			wa.setKeywords(doc.head().select("meta[name=keywords]").attr("content"));
-			wa.setDescription(doc.head().select("meta[name=description]").attr("content"));
+			wa.setKeywords(doc.head().select("meta[name=keywords]")
+					.attr("content"));
+			wa.setDescription(doc.head().select("meta[name=description]")
+					.attr("content"));
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return wa;
 	}
-	
-	public static void main(String[] args)
-	{
-		String url="http://club.autohome.com.cn";
-		//WebTest wt=new WebTest();
-		//System.out.println(wt.getTitle(url));
-		Fetcher f=new Fetcher();
+
+	public void pageInOut(int opt) {
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					System.in));
+			PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+			String line = "";
+			// Set<WordEntry> sws=null;
+			WebAbstract wa = new WebAbstract();
+			while ((line = br.readLine()) != null) {
+				wa = fetcher.getAbstract(line);
+				if (wa == null) {
+					continue;
+				}
+				if (opt == 0) {
+					pw.println(line + "\001" + wa.getTitle());
+				} else if (opt == 1) {
+					pw.println(line + "\001" + wa.toRegularString());
+				}
+
+			}
+
+			br.close();
+			pw.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+
+		if (args.length != 1) {
+			System.err.println("Usage:<opt>" + "opt:" + "0->title"
+					+ "1->title、keywords、description");
+			System.exit(1);
+		}
+
+		int opt=0;
+		opt=Integer.parseInt(args[0]);
+		
+		/*
+		String url = "http://club.autohome.com.cn";
+		// WebTest wt=new WebTest();
+		// System.out.println(wt.getTitle(url));
+		Fetcher f = new Fetcher();
 		System.out.println(f.getAbstract(url).toString());
+		*/
+		WebTest wt=new WebTest();
+		wt.pageInOut(opt);
+		
+
+	}
+
+	public Fetcher getFetcher() {
+		return fetcher;
+	}
+
+	public void setFetcher(Fetcher fetcher) {
+		this.fetcher = fetcher;
 	}
 }
