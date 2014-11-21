2be63935898a9b0a98fee77fe9e812e593f26e51
clickwise <strongmind666@126.com>
2014-11-21 11:57:30 +0800

    run test

--- src/cn/clickwise/clickad/profile/FilePredict.java
+++ src/cn/clickwise/clickad/profile/FilePredict.java
@@ -7,18 +7,16 @@ import java.io.PrintWriter;
 
 import org.jmlp.str.app.SetJar;
 
-
 public class FilePredict {
 
 	private SetJar sd = null;
 
 	private ProfilePredict dpp = null;
 
-	public FilePredict()
-	{
-	  init();	
+	public FilePredict() {
+		init();
 	}
-	
+
 	public void init() {
 		sd = new SetJar();
 		sd.init_first();
@@ -50,10 +48,13 @@ public class FilePredict {
 	public static void main(String[] args) {
 
 		if (args.length != 3) {
-			System.err.println("Usage:<field_num> <key_field_index> <separator>");
+			System.err
+					.println("Usage:<field_num> <key_field_index> <separator>");
 			System.err.println("    field_num : 输入的字段个数");
-			System.err.println("    key_field_index: 要进行关键词提取的字段编号，从0开始，即0表示第一个字段");
-			System.err.println("    separator:字段间的分隔符，001 表示 字符001，blank 表示\\s+ 即连续空格,tab 表示 \t");
+			System.err
+					.println("    key_field_index: 要进行关键词提取的字段编号，从0开始，即0表示第一个字段");
+			System.err
+					.println("    separator:字段间的分隔符，001 表示 字符001，blank 表示\\s+ 即连续空格,tab 表示 \t");
 			System.exit(1);
 		}
 
@@ -84,6 +85,7 @@ public class FilePredict {
 			outputSeparator = separator.trim();
 		}
 
+		System.err.println("fieldNum:"+fieldNum+" keyFieldIndex:"+keyFieldIndex+" sep:"+args[2]);
 		FilePredict fp = new FilePredict();
 
 		InputStreamReader isr = new InputStreamReader(System.in);
@@ -98,32 +100,36 @@ public class FilePredict {
 		try {
 			while ((line = br.readLine()) != null) {
 
-				System.err.println("process line :"+line);
-				fields = line.split(separator);
-				if (fields.length != fieldNum) {
-					continue;
-				}
+				try {
+					System.err.println("process line :" + line);
+					fields = line.split(separator);
+					if (fields.length != fieldNum) {
+						continue;
+					}
 
-				for (int j = 0; j < keyFieldIndex; j++) {
-					pw.print(fields[j] + outputSeparator);
-				}
+					for (int j = 0; j < keyFieldIndex; j++) {
+						pw.print(fields[j] + outputSeparator);
+					}
 
-				if (keyFieldIndex < (fieldNum - 1)) {
-					pw.print(fp.pline(fields[keyFieldIndex]).trim()
-							+ outputSeparator);
-				} else {
-					pw.print(fp.pline(fields[keyFieldIndex]).trim());
-				}
+					if (keyFieldIndex < (fieldNum - 1)) {
+						pw.print(fp.pline(fields[keyFieldIndex]).trim()
+								+ outputSeparator);
+					} else {
+						pw.print(fp.pline(fields[keyFieldIndex]).trim());
+					}
 
-				for (int j = keyFieldIndex + 1; j < fieldNum - 1; j++) {
-					pw.println(fields[j] + outputSeparator);
-				}
+					for (int j = keyFieldIndex + 1; j < fieldNum - 1; j++) {
+						pw.println(fields[j] + outputSeparator);
+					}
 
-				if (keyFieldIndex < (fieldNum - 1)) {
-					// pw.print(ke.keyword_extract_noun(fields[fieldNum-1]));
-					pw.print(fields[fieldNum - 1]);
+					if (keyFieldIndex < (fieldNum - 1)) {
+						// pw.print(ke.keyword_extract_noun(fields[fieldNum-1]));
+						pw.print(fields[fieldNum - 1]);
+					}
+					pw.println();
+				} catch (Exception e) {
+					e.printStackTrace();
 				}
-				pw.println();
 			}
 
 			isr.close();
