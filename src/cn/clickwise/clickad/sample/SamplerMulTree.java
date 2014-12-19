6951d33f5957acda6e62ab883174ad987929c1de
clickwise <strongmind666@126.com>
2014-12-19 10:04:05 +0800

    run test

--- src/cn/clickwise/clickad/sample/SamplerMulTree.java
+++ src/cn/clickwise/clickad/sample/SamplerMulTree.java
@@ -20,128 +20,119 @@ public class SamplerMulTree {
 	 * 统计单词出现的次数
 	 */
 	private MapCount<String> dictCounts;
-	
+
 	/**
 	 * 统计label出现的次数
 	 */
 	private MapCount<String> labelCounts;
-	
+
 	/**
 	 * 建立每个标记的各层索引，索引是在其父节点各子节点中的编号
 	 */
-	private HashMap<String,HashMap<String,HashMap<String,String>>> labelDicts;
-	
+	private HashMap<String, HashMap<String, HashMap<String, String>>> labelDicts;
+
 	/**
 	 * 词典索引
 	 */
-	private HashMap<String,Integer> dicts;
-	
+	private HashMap<String, Integer> dicts;
+
 	/**
 	 * label索引
 	 */
-	private HashMap<String,Integer> labels;
-	
-	public SamplerMulTree(){
-		dictCounts=new MapCount<String>();
-		labelCounts=new MapCount<String>();
-		labelDicts=new HashMap<String,HashMap<String,HashMap<String,String>>>();
-		dicts=new HashMap<String,Integer>();
-		labels=new HashMap<String,Integer>();
+	private HashMap<String, Integer> labels;
+
+	public SamplerMulTree() {
+		dictCounts = new MapCount<String>();
+		labelCounts = new MapCount<String>();
+		labelDicts = new HashMap<String, HashMap<String, HashMap<String, String>>>();
+		dicts = new HashMap<String, Integer>();
+		labels = new HashMap<String, Integer>();
 	}
-	
-	public void getDictsAndLabels(int field_num,int sample_field_index,int label_field_index,String separator,ArrayList<String> docs){
-		
-		String line="";
-		String[] fields=null;
-		String label="";
-		String text="";
-		String[] tokens=null;
-		
+
+	public void getDictsAndLabels(int field_num, int sample_field_index,
+			int label_field_index, String separator, ArrayList<String> docs) {
+
+		String line = "";
+		String[] fields = null;
+		String label = "";
+		String text = "";
+		String[] tokens = null;
+
 		int dict_index = 1;
 		int label_index = 1;
-		String token="";
-		
-		String[] cates=null;
-		String fcate="";
-		String scate="";
-		String tcate="";
-		
-		for(int i=0;i<docs.size();i++)
-		{
-			line=docs.get(i);
-			fields=line.split(separator);
-			if(fields.length!=field_num)
-			{
+		String token = "";
+
+		String[] cates = null;
+		String fcate = "";
+		String scate = "";
+		String tcate = "";
+
+		for (int i = 0; i < docs.size(); i++) {
+			line = docs.get(i);
+			fields = line.split(separator);
+			if (fields.length != field_num) {
 				continue;
 			}
-			
-		    label=fields[label_field_index];
-		    text=fields[sample_field_index];
-		    if(SSO.tioe(label))
-		    {
-		    	continue;
-		    }
-		    label=label.trim();
-		    
-		    labelCounts.add(label);
-		    if(!(labels.containsKey(label)))
-		    {
-		    	labels.put(label, label_index++);
-		    }
-		
-		    cates=label.split("\\|");
-		    if(cates.length!=3)
-		    {
-		    	continue;
-		    }
-		    fcate=cates[0].trim();
-		    scate=cates[1].trim();
-		    tcate=cates[2].trim();
-		    
-		    if(!(labelDicts.containsKey(fcate)))
-		    {
-		    	labelDicts.put(fcate, new HashMap<String,HashMap<String,String>>());
-		    	if(!(labelDicts.get(fcate).containsKey(scate)))
-		    	{
-		    		labelDicts.get(fcate).put(scate, new HashMap<String,String>());
-		    	}
-		    	
-		    	if(!(labelDicts.get(fcate).get(scate).containsKey(tcate)))
-		    	{
-		    		labelDicts.get(fcate).get(scate).put(tcate, labelDicts.get(fcate).size()+1+"|"+labelDicts.get(fcate).get(scate).size()+1+"|"+labelDicts.get(fcate).get(scate).get(tcate)+1);
-		    	}   	
-		    }
-		    else
-		    {
-		    	
-		    }
-		    
-		    if(SSO.tioe(text))
-		    {
-		    	continue;
-		    }
-		    text=text.trim();
-		    
-		    tokens=text.split("\\s+");
-		    for(int j=0;j<tokens.length;j++)
-		    {
-		      token=tokens[j];
-		      if(SSO.tioe(token))
-		      {
-		    	  continue;
-		      }
-		      token=token.trim();
-		      dictCounts.add(token);
-			  if(!(dicts.containsKey(token)))
-			  {
-			    dicts.put(token, dict_index++);
+
+			label = fields[label_field_index];
+			text = fields[sample_field_index];
+			if (SSO.tioe(label)) {
+				continue;
+			}
+			label = label.trim();
+
+			labelCounts.add(label);
+			if (!(labels.containsKey(label))) {
+				labels.put(label, label_index++);
+			}
+
+			cates = label.split("\\|");
+			if (cates.length != 3) {
+				continue;
+			}
+			fcate = cates[0].trim();
+			scate = cates[1].trim();
+			tcate = cates[2].trim();
+
+			if (!(labelDicts.containsKey(fcate))) {
+				labelDicts.put(fcate,new HashMap<String, HashMap<String, String>>());
+			}
+			else
+			{
+			  if (!(labelDicts.get(fcate).containsKey(scate))) {
+				labelDicts.get(fcate).put(scate, new HashMap<String, String>());
 			  }
-		    }
-		    
+			  else
+			  {
+			    if (!(labelDicts.get(fcate).get(scate).containsKey(tcate))) {
+				   labelDicts.get(fcate).get(scate).put(tcate,(labelDicts.get(fcate).size()+ 1)+ "|"+ (labelDicts.get(fcate).get(scate).size()+ 1)+ "|"+ (labelDicts.get(fcate).get(scate).get(tcate) + 1));
+			    }
+			  }
+			}
+			
+			
+			if (SSO.tioe(text)) {
+				continue;
+			}
+			text = text.trim();
+
+			tokens = text.split("\\s+");
+			for (int j = 0; j < tokens.length; j++) {
+				token = tokens[j];
+				if (SSO.tioe(token)) {
+					continue;
+				}
+				token = token.trim();
+				dictCounts.add(token);
+				if (!(dicts.containsKey(token))) {
+					dicts.put(token, dict_index++);
+				}
+			}
+
 		}
-		
+
 	}
-	
+
 	public String get_word_id(String s) {
 		String words[] = s.split("\\s+");
 		String res = "";
@@ -193,199 +184,277 @@ public class SamplerMulTree {
 		}
 		return res;
 	}
-	
-	public void train2sample(int field_num,int sample_field_index,int label_field_index,String separator,String outputSeparator,String gendict,String genlabeldict,String gensample,ArrayList<String> docs)
-	{
-		getDictsAndLabels(field_num,sample_field_index, label_field_index,separator,docs);
-		
-		String line="";
-		String[] fields=null;
-		String label="";
-		String text="";
-		PrintWriter gspw=null;
-		try{	
-		gspw=new PrintWriter(new FileWriter(gensample));
-		int labelIndex=0;
-		String forsample="";
-		
-		for(int i=0;i<docs.size();i++)
-		{
-			line=docs.get(i);
-			fields=line.split(separator);
-			if(fields.length!=field_num)
-			{
-				continue;
+
+	public void train2sample(int field_num, int sample_field_index,
+			int label_field_index, String separator, String outputSeparator,
+			String gendict, String genlabeldict, String gensample,
+			ArrayList<String> docs) {
+		getDictsAndLabels(field_num, sample_field_index, label_field_index,
+				separator, docs);
+
+		String line = "";
+		String[] fields = null;
+		String label = "";
+		String text = "";
+		PrintWriter gspw = null;
+		try {
+			gspw = new PrintWriter(new FileWriter(gensample));
+			int labelIndex = 0;
+			String forsample = "";
+
+			for (int i = 0; i < docs.size(); i++) {
+				line = docs.get(i);
+				fields = line.split(separator);
+				if (fields.length != field_num) {
+					continue;
+				}
+
+				label = fields[label_field_index];
+				text = fields[sample_field_index];
+				if (SSO.tioe(label)) {
+					continue;
+				}
+				label = label.trim();
+				labelIndex = 0;
+				labelIndex = labels.get(label);
+				if (labelIndex < 1) {
+					continue;
+				}
+
+				if (SSO.tioe(text)) {
+					continue;
+				}
+				text = text.trim();
+				forsample = get_word_id(text);
+				if (SSO.tioe(forsample)) {
+					continue;
+				}
+				forsample = forsample.trim();
+				gspw.println(labelIndex + outputSeparator + forsample);
 			}
-			
-		    label=fields[label_field_index];
-		    text=fields[sample_field_index];
-		    if(SSO.tioe(label))
-		    {
-		    	continue;
-		    }
-		    label=label.trim();
-		    labelIndex=0;
-		    labelIndex=labels.get(label);
-		    if(labelIndex<1)
-		    {
-		    	continue;
-		    }
-		    
-		    if(SSO.tioe(text))
-		    {
-		    	continue;
-		    }
-		    text=text.trim();
-		    forsample=get_word_id(text);
-		    if(SSO.tioe(forsample))
-		    {
-		    	continue;
-		    }
-		    forsample=forsample.trim();
-		    gspw.println(labelIndex+outputSeparator+forsample);
+			gspw.close();
+			printDicts(gendict, outputSeparator);
+			printLabelDicts(genlabeldict, outputSeparator);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		 gspw.close();
-		 printDicts(gendict,outputSeparator);
-		 printLabelDicts(genlabeldict,outputSeparator);
-		}catch(Exception e){
-		  e.printStackTrace();	
-		}
+
+	}
+
+	public void train2mulsample(int field_num, int sample_field_index,
+			int label_field_index, String separator, String outputSeparator,
+			String gendict, String genlabeldict, String gensample,
+			ArrayList<String> docs) {
 		
+		getDictsAndLabels(field_num, sample_field_index, label_field_index,
+				separator, docs);
+
+		String line = "";
+		String[] fields = null;
+		String label = "";
+		String text = "";
+		PrintWriter gspw = null;
+		try {
+			
+			gspw = new PrintWriter(new FileWriter(gensample));
+			//int labelIndex = 0;
+			String forsample = "";
+
+			String labelIndexStr="";
+			String[] cates=null;
+			String fcate="";
+			String scate="";
+			String tcate="";
+			
+			for (int i = 0; i < docs.size(); i++) {
+				line = docs.get(i);
+				fields = line.split(separator);
+				if (fields.length != field_num) {
+					continue;
+				}
+
+				label = fields[label_field_index];
+				text = fields[sample_field_index];
+				if (SSO.tioe(label)) {
+					continue;
+				}
+				label = label.trim();
+				cates=label.split("\\|");
+				if(cates.length!=3)
+				{
+					continue;
+				}
+				
+				fcate=cates[0].trim();
+				scate=cates[1].trim();
+				tcate=cates[2].trim();
+				labelIndexStr=labelDicts.get(fcate).get(scate).get(tcate);
+				/*
+				labelIndex = 0;
+				labelIndex = labels.get(label);
+				if (labelIndex < 1) {
+					continue;
+				}
+                */
+				
+				if (SSO.tioe(text)) {
+					continue;
+				}
+				text = text.trim();
+				
+				forsample = get_word_id(text);
+				if (SSO.tioe(forsample)) {
+					continue;
+				}
+				forsample = forsample.trim();
+				gspw.println(labelIndexStr + outputSeparator + forsample);
+			}
+			gspw.close();
+			
+			printDicts(gendict, outputSeparator);
+			printMulLabelDicts(genlabeldict, outputSeparator);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 	
-	public void printDicts(String gendict,String outputSeparator)
-	{
-		try{
-			PrintWriter gdpw=new PrintWriter(new FileWriter(gendict));
-			for(Map.Entry<String, Integer> e:dicts.entrySet())
-			{
-				gdpw.println(e.getKey()+outputSeparator+e.getValue());
+	public void printDicts(String gendict, String outputSeparator) {
+		try {
+			PrintWriter gdpw = new PrintWriter(new FileWriter(gendict));
+			for (Map.Entry<String, Integer> e : dicts.entrySet()) {
+				gdpw.println(e.getKey() + outputSeparator + e.getValue());
 			}
 			gdpw.close();
-		}
-		catch(Exception e)
-		{
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public void printLabelDicts(String genlabeldict,String outputSeparator)
-	{
-		try{
-			PrintWriter glpw=new PrintWriter(new FileWriter(genlabeldict));
-			for(Map.Entry<String, Integer> e:labels.entrySet())
-			{
-				glpw.println(e.getKey()+outputSeparator+e.getValue());
+
+	public void printLabelDicts(String genlabeldict, String outputSeparator) {
+		try {
+			PrintWriter glpw = new PrintWriter(new FileWriter(genlabeldict));
+			for (Map.Entry<String, Integer> e : labels.entrySet()) {
+				glpw.println(e.getKey() + outputSeparator + e.getValue());
 			}
 			glpw.close();
-		}
-		catch(Exception e)
-		{
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
-	public static void main(String[] args) throws Exception
-	{
-	
-		if(args.length!=7)
-		{
-			System.err.println("Usage:<field_num> <sample_field_index> <label_field_index> <separator> <gendict> <genlabeldict> <gensample>");
+	public void printMulLabelDicts(String genlabeldict, String outputSeparator) {
+		try {
+			PrintWriter glpw = new PrintWriter(new FileWriter(genlabeldict));
+			for (Map.Entry<String, HashMap<String, HashMap<String, String>>> e1 : labelDicts.entrySet()) {
+				for(Map.Entry<String, HashMap<String, String>> e2: e1.getValue().entrySet())
+				{
+					for(Map.Entry<String, String> e3:e2.getValue().entrySet())
+					{
+						glpw.println(e3.getValue());
+					}
+				}	
+			}
+			glpw.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+
+		if (args.length != 7) {
+			System.err
+					.println("Usage:<field_num> <sample_field_index> <label_field_index> <separator> <gendict> <genlabeldict> <gensample>");
 			System.err.println("    field_num : 输入的字段个数");
-			System.err.println("    sample_field_index: 样本体所在的字段，从0开始，即0表示第一个字段");
+			System.err
+					.println("    sample_field_index: 样本体所在的字段，从0开始，即0表示第一个字段");
 			System.err.println("    label_field_index: 标记所在的字段，从0开始，即0表示第一个字段");
-			System.err.println("    separator:字段间的分隔符，001 表示 字符001，blank 表示\\s+ 即连续空格 ,tab 表示 \t");
+			System.err
+					.println("    separator:字段间的分隔符，001 表示 字符001，blank 表示\\s+ 即连续空格 ,tab 表示 \t");
 			System.err.println("    gendict:生成的词典路径,从1开始");
 			System.err.println("    genlabeldict:生成的标记索引路径，从1开始");
 			System.err.println("    gensample:生成的样本路径");
 			System.exit(1);
 		}
-		
-		
-		//输入的字段个数用
-		int fieldNum=0;
-		
-		//样本体所在的字段编号
-		int sampleFieldIndex=0;
-		
-		int labelFieldIndex=0;
-		
-		//字段间的分隔符:001 表示 \001
-		//             :blank 表示\\s+ 即连续空格
-		String separator="";
-		String outputSeparator="";
-		
-		String gendict="";
-		String genlabeldict="";
-		String gensample="";
-		
-		fieldNum=Integer.parseInt(args[0]);
-		sampleFieldIndex=Integer.parseInt(args[1]);
-		labelFieldIndex=Integer.parseInt(args[2]);
-		
-		if(args[3].equals("001"))
-		{
-			separator="\001";
-			outputSeparator="\001";
+
+		// 输入的字段个数用
+		int fieldNum = 0;
+
+		// 样本体所在的字段编号
+		int sampleFieldIndex = 0;
+
+		int labelFieldIndex = 0;
+
+		// 字段间的分隔符:001 表示 \001
+		// :blank 表示\\s+ 即连续空格
+		String separator = "";
+		String outputSeparator = "";
+
+		String gendict = "";
+		String genlabeldict = "";
+		String gensample = "";
+
+		fieldNum = Integer.parseInt(args[0]);
+		sampleFieldIndex = Integer.parseInt(args[1]);
+		labelFieldIndex = Integer.parseInt(args[2]);
+
+		if (args[3].equals("001")) {
+			separator = "\001";
+			outputSeparator = "\001";
+		} else if (args[3].equals("blank")) {
+			separator = "\\s+";
+			outputSeparator = "\t";
+		} else if (args[3].equals("tab")) {
+			separator = "\t";
+			outputSeparator = "\t";
+		} else {
+			separator = args[3].trim();
+			outputSeparator = separator.trim();
 		}
-		else if(args[3].equals("blank"))
-		{
-			separator="\\s+";
-			outputSeparator="\t";
+
+		gendict = args[4].trim();
+		genlabeldict = args[5].trim();
+		gensample = args[6].trim();
+
+		SamplerMulTree sampler = new SamplerMulTree();
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(isr);
+
+		// String line="";
+		// while((line=br.readLine())!=null)
+		// {
+		// pw.println(posTagger.tag(line));
+		// }
+
+		String line = "";
+		String[] fields = null;
+		ArrayList<String> docs = new ArrayList<String>();
+		while ((line = br.readLine()) != null) {
+			if (SSO.tioe(line)) {
+				continue;
+			}
+			line = line.trim();
+			docs.add(line);
 		}
-		else if(args[3].equals("tab"))
-		{
-			separator="\t";
-			outputSeparator="\t";
-		}
-		else
-		{
-			separator=args[3].trim();
-			outputSeparator=separator.trim();
-		}	
-		
-		gendict=args[4].trim();
-		genlabeldict=args[5].trim();
-		gensample=args[6].trim();
-		
-		Sampler sampler=new Sampler();
-		InputStreamReader isr=new InputStreamReader(System.in);
-		BufferedReader br=new BufferedReader(isr);
-		
-		
-		//String line="";
-		//while((line=br.readLine())!=null)
-		//{
-		//	pw.println(posTagger.tag(line));
-		//}
-		
-		String line="";
-		String[] fields=null;
-		ArrayList<String> docs=new ArrayList<String>();
-		while((line=br.readLine())!=null)
-		{
-		   if(SSO.tioe(line))
-		   {
-			   continue;
-		   }
-		   line=line.trim();
-		   docs.add(line);
-		}
-		
+
 		isr.close();
 		br.close();
-		
-		sampler.train2sample(fieldNum, sampleFieldIndex,labelFieldIndex, separator, outputSeparator, gendict, genlabeldict, gensample, docs);
-		
+
+		sampler.train2mulsample(fieldNum, sampleFieldIndex, labelFieldIndex,
+				separator, outputSeparator, gendict, genlabeldict, gensample,
+				docs);
+
 	}
 
-	public HashMap<String,HashMap<String,HashMap<String,String>>> getLabelDicts() {
+	public HashMap<String, HashMap<String, HashMap<String, String>>> getLabelDicts() {
 		return labelDicts;
 	}
 
-	public void setLabelDicts(HashMap<String,HashMap<String,HashMap<String,String>>> labelDicts) {
+	public void setLabelDicts(
+			HashMap<String, HashMap<String, HashMap<String, String>>> labelDicts) {
 		this.labelDicts = labelDicts;
 	}
-
 
 }
