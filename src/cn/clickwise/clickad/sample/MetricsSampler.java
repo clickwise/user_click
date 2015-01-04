c71df6bee718ae135367e6766dda6356865d0048
bluesky <strongmind666@126.com>
2015-01-05 00:38:54 +0800

    run test

--- src/cn/clickwise/clickad/sample/MetricsSampler.java
+++ src/cn/clickwise/clickad/sample/MetricsSampler.java
@@ -122,7 +122,7 @@ public class MetricsSampler {
 			ArrayList<WORD> swlist=m.getValue();
 			for(int i=0;i<swlist.size();i++)
 			{
-				logger.info(swlist.get(i).w+":"+swlist.get(i).v);
+				logger.info(m+"\t"+swlist.get(i).w+":"+swlist.get(i).v);
 			}
 			
 		}
