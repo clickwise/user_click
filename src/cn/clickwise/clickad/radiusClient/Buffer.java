b339ad92f3656f7808d2c10ab126478915370255
clickwise <strongmind666@126.com>
2015-01-16 10:02:30 +0800

    run test

---
+++ src/cn/clickwise/clickad/radiusClient/Buffer.java
@@ -0,0 +1,13 @@
+package cn.clickwise.clickad.radiusClient;
+/**
+ * 各种buffer 数组
+ * @author zkyz
+ */
+public class Buffer {
+
+	//接收packet 的头信息
+	public static byte[] head = new byte[16];
+	
+	
+	
+}
