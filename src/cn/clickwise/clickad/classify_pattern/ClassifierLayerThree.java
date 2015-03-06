f0b275fe1be9f848d665c77002488eb9f8d6d1ee
clickwise <strongmind666@126.com>
2015-03-06 09:53:29 +0800

    run test

--- src/cn/clickwise/clickad/classify_pattern/ClassifierLayerThree.java
+++ src/cn/clickwise/clickad/classify_pattern/ClassifierLayerThree.java
@@ -28,14 +28,14 @@ public class ClassifierLayerThree extends Classifier{
 		super();
 		System.out.println("initialize layer three model");
 		try {
-			String model_path = "model_vvjj.txt";
+			String model_path = "eclo_model";
 			model=read_model(model_path);
 			seg = new Segmenter();
 			posTagger = new PosTagger("chinese-nodistsim.tagger");
 			ke = new KeyExtract();
-			video_dict = getDictFromStream("gendict_vvjj.txt");
-			label_names = getIndexLabelFromStream("genlabeldict_vvjj.txt");
-			readPossLabels("genlabeldict_vvjj.txt");
+			video_dict = getDictFromStream("eclo_dict.txt");
+			label_names = getIndexLabelFromStream("eclo_labeldict.txt");
+			readPossLabels("eclo_labeldict.txt");
 			String text="睡袋 户外加宽加厚保暖 秋冬季超轻成人睡袋野营可拼接双人睡袋 ";
 			System.out.println("cate:"+cate(text));
 		} catch (Exception e) {
@@ -48,16 +48,16 @@ public class ClassifierLayerThree extends Classifier{
 		super();
 		System.out.println("initialize layer three model");
 		try {
-			String model_path = "model_vvjj.txt";
+			String model_path = "eclo_model";
 			model=read_model(model_path);
 			seg = new Segmenter();
 			System.err.println("loading dict "+dict);
 			seg.loadAnsjDic(new File(dict));
 			posTagger = new PosTagger("chinese-nodistsim.tagger");
 			ke = new KeyExtract();
-			video_dict = getDictFromStream("gendict_vvjj.txt");
-			label_names = getIndexLabelFromStream("genlabeldict_vvjj.txt");
-			readPossLabels("genlabeldict_vvjj.txt");
+			video_dict = getDictFromStream("eclo_dict.txt");
+			label_names = getIndexLabelFromStream("eclo_labeldict.txt");
+			readPossLabels("eclo_labeldict.txt");
 			String text="睡袋 户外加宽加厚保暖 秋冬季超轻成人睡袋野营可拼接双人睡袋 ";
 			System.out.println("cate:"+cate(text));
 		} catch (Exception e) {
