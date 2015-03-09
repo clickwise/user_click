e0d8661a110968e0d6b522372d8545b352b77a17
clickwise <strongmind666@126.com>
2015-03-09 16:16:14 +0800

    run test

--- src/cn/clickwise/clickad/hbase/HbaseTest.java
+++ src/cn/clickwise/clickad/hbase/HbaseTest.java
@@ -31,7 +31,7 @@ public class HbaseTest {
     static { 
         configuration = HBaseConfiguration.create(); 
         configuration.set("hbase.zookeeper.property.clientPort", "2181"); 
-        configuration.set("hbase.zookeeper.quorum", "192.168.10.128"); 
+        configuration.set("hbase.zookeeper.quorum", "192.168.10.129"); 
         configuration.set("hbase.master", "192.168.10.128:60010"); 
     } 
  
