b546994cfa399abf33592cde3b9ef9eff1d1d553
clickwise <strongmind666@126.com>
2015-03-23 21:24:28 +0800

    run test

--- src/cn/clickwise/rpc/FileCopyFromClient.java
+++ src/cn/clickwise/rpc/FileCopyFromClient.java
@@ -2,9 +2,7 @@ package cn.clickwise.rpc;
 
 import java.io.BufferedReader;
 import java.io.FileWriter;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
