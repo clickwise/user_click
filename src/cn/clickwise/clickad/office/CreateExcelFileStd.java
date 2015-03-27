a937d8000c9938ec94c15c6aab1537e7b49ab9df
clickwise <strongmind666@126.com>
2015-03-27 11:37:54 +0800

    run test

--- src/cn/clickwise/clickad/office/CreateExcelFileStd.java
+++ src/cn/clickwise/clickad/office/CreateExcelFileStd.java
@@ -7,7 +7,6 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
-
 import cn.clickwise.liqi.str.basic.FileToArray;
 
 public class CreateExcelFileStd {
