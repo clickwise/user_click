727adbab05a3bd974f3c3ba269edd31c1ab35317
clickwise <strongmind666@126.com>
2015-04-02 14:18:34 +0800

    run test

--- src/cn/clickwise/clickad/jd_opinion/SEClickMR.java
+++ src/cn/clickwise/clickad/jd_opinion/SEClickMR.java
@@ -4,13 +4,23 @@ import java.io.IOException;
 import java.util.Iterator;
 import java.util.Vector;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.Mapper.Context;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.NullOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
 
 import cn.clickwise.lib.string.SSO;
+import cn.clickwise.smartjobs.TestMR;
+import cn.clickwise.smartjobs.TestMR.PrepareMapper;
+import cn.clickwise.smartjobs.TestMR.PrepareReducer;
 
 public class SEClickMR {
 
@@ -78,6 +88,34 @@ public class SEClickMR {
 	        }
 			context.write(key, new IntWritable(sum));
 		}
+	}
+	
+	
+	public static void main(String[] args) throws Exception {
+
+		Configuration conf = new Configuration();
+		String[] otherArgs = new GenericOptionsParser(conf, args)
+				.getRemainingArgs();
+		if (otherArgs.length != 3) {
+			System.err.println("Usage: TestMR <day> <input> <output>");
+			System.exit(2);
+		}
+
+		String day = otherArgs[0];
+		Job job = new Job(conf, "TestMR_" + day);
+		job.setJarByClass(TestMR.class);
+
+		job.setMapperClass(PPrepareMapper.class);
+		job.setReducerClass(PPrepareReducer.class);
+		job.setNumReduceTasks(1);
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(Text.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(NullOutputFormat.class);
+		FileInputFormat.addInputPath(job, new Path(otherArgs[1]));
+		FileOutputFormat.setOutputPath(job, new Path(otherArgs[2]));
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+		
 	}
 
 }
--- src/cn/clickwise/clickad/mapreduce/WordCount.java
+++ src/cn/clickwise/clickad/mapreduce/WordCount.java
@@ -89,7 +89,7 @@ public class WordCount {
         conf.set("yarn.resourcemanager.address", "${yarn.resourcemanager.hostname}:8032");
         conf.set("yarn.resourcemanager.resource-tracker.address", "${yarn.resourcemanager.hostname}:8031");
         conf.set("yarn.resourcemanager.scheduler.address", "${yarn.resourcemanager.hostname}:8030");
- 
+        conf.set("yarn.ipc.rpc.class", "org.apache.hadoop.yarn.ipc.HadoopYarnProtoRPC");
         String[] otherArgs = new String[2];
         otherArgs[0] = "hdfs://zjdx107:8020/user/clickwise/test_dir/t.txt";//计算原文件目录，需提前在里面存入文件
         String time = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
