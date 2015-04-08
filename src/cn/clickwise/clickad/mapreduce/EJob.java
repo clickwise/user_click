d60dd8b79293d4838c8c83cc032f91bcd9268007
clickwise <strongmind666@126.com>
2015-04-08 14:13:28 +0800

    run test

--- src/cn/clickwise/clickad/mapreduce/EJob.java
+++ src/cn/clickwise/clickad/mapreduce/EJob.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.jar.JarEntry;
 import java.util.jar.JarOutputStream;
