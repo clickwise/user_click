0cdf005c697d87d7881d98fa3bf8064a4ffe15dd
bluesky <strongmind666@126.com>
2015-04-09 21:30:38 +0800

    run test

---
+++ src/cn/clickwise/clickad/hbase/IP2Radius.java
@@ -0,0 +1,131 @@
+package cn.clickwise.clickad.hbase;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import cn.clickwise.lib.string.SSO;
+
+public class IP2Radius {
+
+	private HashMap<String,Integer> ips=new HashMap<String,Integer>();
+	
+	/**
+	 * 读取批量ip
+	 * @param ips_file
+	 */
+	public void readIps(String ips_file)
+	{
+		String line="";
+		
+		try{
+			
+			BufferedReader br=new BufferedReader(new FileReader(ips_file));
+			
+			while((line=br.readLine())!=null)
+			{
+				if(SSO.tioe(line))
+				{
+					continue;
+				}
+				
+				if(!(ips.containsKey(line)))
+				{
+				    ips.put(line, 1);	
+				}	
+			}
+			
+			br.close();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public HashMap<String,HashMap<String,Integer>> getRadiusForIp()
+	{
+		
+		HashMap<String,HashMap<String,Integer>> ipradius=null;
+		try{
+			
+			ipradius=new HashMap<String,HashMap<String,Integer>>();
+		   	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			String line="";
+			
+			String[] fields=null;
+			String ip="";
+			String radiusId="";
+			String status="";
+			String time="";
+			
+			while((line=br.readLine())!=null)
+			{
+				if(SSO.tioe(line))
+				{
+					continue;
+				}
+				
+				fields=line.split("\t");
+				
+				if(fields.length!=4)
+				{
+					continue;
+				}
+				
+				ip=fields[0];
+				radiusId=fields[1];
+				status=fields[2];
+				time=fields[3];
+			
+				if(!(ipradius.containsKey(ip)))
+				{
+					ipradius.put(ip, new HashMap<String,Integer>());
+					ipradius.get(ip).put(radiusId, 1);
+				}
+				else if(!(ipradius.get(ip).containsKey(radiusId)))
+				{
+					ipradius.get(ip).put(radiusId, 1);
+				}
+			}
+			
+			
+		   	PrintWriter pw=new PrintWriter(new OutputStreamWriter(System.out));
+		   	
+		   	for(Map.Entry<String, HashMap<String,Integer>> ipr:ipradius.entrySet())
+		   	{
+		   		if(ipr.getValue()==null)
+		   		{
+		   			continue;
+		   		}
+		   		
+		   		String radiusIdStr="";
+		   		for(Map.Entry<String, Integer> ipre:ipr.getValue().entrySet())
+		   		{
+		   			radiusIdStr=radiusIdStr+ipre.getKey()+"\t";
+		   		}
+		   		
+		   		pw.println(ipr.getKey()+"\001"+radiusIdStr);
+		   	}
+
+		   	
+		   	pw.close();
+		   	
+			
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		
+		return ipradius;
+	}
+	
+}
