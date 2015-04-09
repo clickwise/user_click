bbc487941fb8b711590da3abb14316dd51274af5
clickwise <strongmind666@126.com>
2015-04-09 10:04:36 +0800

    run test

--- src/cn/clickwise/clickad/hbase/HbaseTest.java
+++ src/cn/clickwise/clickad/hbase/HbaseTest.java
@@ -39,14 +39,14 @@ public class HbaseTest {
         
         /***********local*******************/
         configuration.set("hbase.zookeeper.property.clientPort", "2181"); 
-        configuration.set("hbase.zookeeper.quorum", "192.168.110.81"); 
-        configuration.set("hbase.master", "192.168.110.81:60000"); 
+        configuration.set("hbase.zookeeper.quorum", "192.168.110.80"); 
+        configuration.set("hbase.master", "192.168.110.80:60000"); 
         /************************************/
     } 
  
     public static void main(String[] args) { 
-         createTable("dddwujintao"); 
-         insertData("dddwujintao"); 
+         createTable("edwujintao"); 
+         insertData("edwujintao"); 
         // QueryAll("wujintao"); 
         // QueryByCondition1("wujintao"); 
         // QueryByCondition2("wujintao"); 
