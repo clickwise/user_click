c9efe3495fc5d23bd57f2c17ce7a72b291058189
clickwise <strongmind666@126.com>
2015-04-10 17:19:25 +0800

    run test

--- src/cn/clickwise/clickad/hbase/IP2Radius.java
+++ src/cn/clickwise/clickad/hbase/IP2Radius.java
@@ -136,6 +136,103 @@ public class IP2Radius {
 		return ipradius;
 	}
 	
+	public HashMap<String,HashMap<String,Integer>> getRadiusForIp2()
+	{
+		
+		HashMap<String,HashMap<String,Integer>> ipradius=null;
+		try{
+			
+			ipradius=new HashMap<String,HashMap<String,Integer>>();
+		   	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			String line="";
+			
+			String[] fields=null;
+			String ip="";
+			String radiusId="";
+			String status="";
+			String time="";
+			
+			while((line=br.readLine())!=null)
+			{
+				if(SSO.tioe(line))
+				{
+					continue;
+				}
+				
+				fields=line.split("\\s+");
+				
+				if(fields.length<1)
+				{
+					continue;
+				}
+				
+				ip=fields[0];
+
+			
+				if(!(ips.containsKey(ip)))
+				{
+					continue;
+				}
+				
+				String[] tokens=null;
+				for(int k=1;k<fields.length;k++)
+				{
+				  tokens=fields[k].split(":");
+				  if(tokens.length!=3)
+				  {
+					  continue;
+				  }
+				  
+				  radiusId=tokens[1];
+				  if(!(ipradius.containsKey(ip)))
+				  {
+					ipradius.put(ip, new HashMap<String,Integer>());
+					ipradius.get(ip).put(radiusId, 1);
+				  }
+				  else if(!(ipradius.get(ip).containsKey(radiusId)))
+				  {
+					if((ipradius.get(ip).size())>500)
+					{
+						continue;
+					}
+					ipradius.get(ip).put(radiusId, 1);
+				 }
+				}
+				
+				
+			}
+			
+			
+		   	PrintWriter pw=new PrintWriter(new OutputStreamWriter(System.out));
+		   	
+		   	for(Map.Entry<String, HashMap<String,Integer>> ipr:ipradius.entrySet())
+		   	{
+		   		if(ipr.getValue()==null)
+		   		{
+		   			continue;
+		   		}
+		   		
+		   		String radiusIdStr="";
+		   		for(Map.Entry<String, Integer> ipre:ipr.getValue().entrySet())
+		   		{
+		   			radiusIdStr=radiusIdStr+ipre.getKey()+"\t";
+		   		}
+		   		
+		   		pw.println(ipr.getKey()+"\001"+radiusIdStr);
+		   	}
+   	
+		   	pw.close();
+		   	
+			
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		
+		return ipradius;
+	}
 	
 	public static void main(String[] args)
 	{
@@ -148,7 +245,7 @@ public class IP2Radius {
 		String ips_file=args[0];
 		IP2Radius ip2r=new IP2Radius();
 		ip2r.readIps(ips_file);
-		ip2r.getRadiusForIp();
+		ip2r.getRadiusForIp2();
 		
 	}
 }
